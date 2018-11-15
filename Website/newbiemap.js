140272f603fbc37075a8ba3d9e7aca5a831be3ba
wschoeck <wschoeck@bluewin.ch>
2018-11-15 19:04:26 +0100

    Layer2 und Layer3 erstellt

--- Website/newbiemap.js
+++ Website/newbiemap.js
@@ -1,6 +1,7 @@
 scaleItem = function() {
   console.log("Hallo");
   document.getElementById("firstA").classList.add("zoomen");
+  document.getElementById("firstAbackground").classList.add("zoomen");
   document.getElementById("firstA").classList.add("zindex");
   kreisminus.classList.remove("hidden");
   kreisload.classList.remove("plushidden");
@@ -17,6 +18,7 @@ zoomOut = function()
 {
   if(document.getElementById("kreisminus")) {
   firstA.classList.remove("zoomen");
+  document.getElementById("firstAbackground").classList.remove("zoomen");
   kreisminus.classList.add("hidden");
   kreisload.classList.add("plushidden");
   }
@@ -34,6 +36,13 @@ let zoomload = function() {
   document.getElementById("kreisminus").classList.add("hidden");
   setTimeout(function(){
     document.getElementById("kreisloadhidden").classList.remove("loadhidden");
+    document.getElementById("footer").classList.remove("hidden");
+    document.getElementById("layertwoPicture").classList.remove("hidden");
+    document.getElementById("layertwoC").classList.remove("hidden");
+    document.getElementById("typolayertwocopy").classList.remove("hidden");
+    document.getElementById("layertwoIllu").classList.remove("hidden");
+    document.getElementById("layertwoLokal").classList.remove("hidden");
+    document.getElementById("layertwoIlluWein").classList.remove("hidden");
 }, 1000);
   document.getElementById("back").classList.remove("backhidden");
   document.getElementById("typoplus").classList.add("additionhidden");
@@ -52,6 +61,13 @@ let zoomback = function() {
   document.getElementById("kreisload").classList.remove("zoom");
   document.getElementById("kreisloadhidden").classList.add("loadhidden");
   document.getElementById("back").classList.add("backhidden");
+  document.getElementById("layertwoPicture").classList.add("hidden");
+  document.getElementById("layertwoC").classList.add("hidden");
+  document.getElementById("typolayertwocopy").classList.add("hidden");
+  document.getElementById("layertwoIllu").classList.add("hidden");
+  document.getElementById("footer").classList.add("hidden");
+  document.getElementById("layertwoLokal").classList.add("hidden");
+  document.getElementById("layertwoIlluWein").classList.add("hidden");
   setTimeout(function(){
     document.getElementById("typoplus").classList.remove("additionhidden");
     document.getElementById("kreisminus").classList.remove("hidden");
@@ -61,3 +77,48 @@ let zoomback = function() {
 
 
 back.addEventListener("click", zoomback);
+
+
+let kreisloadtwo = document.getElementById("kreisloadtwo");
+
+let zoomloadtwo = function() {
+  document.getElementById("kreisloadtwo").classList.add("zoom");
+  document.getElementById("typoplusthree").classList.add("additionhidden");
+  document.getElementById("footer").classList.add("hidden");
+  setTimeout(function(){
+    document.getElementById("layerthree").classList.remove("hidden");
+    document.getElementById("backthree").classList.remove("backhidden");
+    document.getElementById("typolayertwo").classList.add("loadhidden");
+    document.getElementById("typolayertwocopy").classList.add("loadhidden");
+    document.getElementById("layerthreeC").classList.remove("hidden");
+    document.getElementById("layerthreeD").classList.remove("hidden");
+    document.getElementById("kreisload").classList.add("plushidden");
+}, 1000);
+}
+
+
+kreisloadtwo.addEventListener("click", zoomloadtwo);
+
+
+let backthree = document.getElementById("backthree");
+
+
+let zoombackthree = function() {
+  console.log("Hallo");
+  document.getElementById("kreisloadtwo").classList.remove("zoom");
+  document.getElementById("backthree").classList.add("backhidden");
+  document.getElementById("layerthree").classList.add("hidden");
+  document.getElementById("typolayertwo").classList.remove("loadhidden");
+  document.getElementById("typolayertwocopy").classList.remove("loadhidden");
+  document.getElementById("layerthreeC").classList.add("hidden");
+  document.getElementById("layerthreeD").classList.add("hidden");
+  document.getElementById("kreisload").classList.remove("plushidden");
+  document.getElementById("footer").classList.remove("hidden");
+  setTimeout(function(){
+    document.getElementById("typoplusthree").classList.remove("additionhidden");
+}, 1000);
+
+}
+
+
+backthree.addEventListener("click", zoombackthree);
